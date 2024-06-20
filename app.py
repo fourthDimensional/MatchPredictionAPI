3f5b3d442f422f1fb7afb4f90cf4539d1ab7bfb2
Sam <finepointdog@gmail.com>
2024-06-19 21:45:19 -0500

    dockerization

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import logging
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 from helpers.logging_setup import setup_logging
@@ -14,12 +14,29 @@ setup_logging()
 
 @app.route('/tba/ping', methods=['POST'])
 def test_connection():  # Uses TBA ping webhook to test the connection
+
     logging.info('Received ping from TBA')
 
     return jsonify({'status': 'success'}), 200
 
-@app.route('/tba/new_match', methods=['POST'])
-def log_new_match():
-    logging.info('Received new match from TBA')
+
+# learned there is only one route for a single webhook and I have to handle it based off of the message type
+@app.route('/tba', methods=['POST'])
+def tba_webhook():
+    logging.info(request.json)
+    logging.info('Received new information from TBA')
+
+    match request.json['message_type']:
+        case 'ping':
+            logging.info('Received ping from TBA')
+        case 'match_score':
+            handle_new_match_score(request['message_data'])
+
+    return jsonify({'status': 'success'}), 200
+
+
+def handle_new_match_score(message_json):
+    logging.info('Received new match score from TBA')
+    logging.info(message_json)
 
     return jsonify({'status': 'success'}), 200
--- helpers/logging_setup.py
+++ helpers/logging_setup.py
@@ -1,5 +1,5 @@
 import logging
-from logging.handlers import RotatingFileHandler
+
 
 def setup_logging():
     logger = logging.getLogger()
@@ -8,13 +8,8 @@ def setup_logging():
     formatter = logging.Formatter('%(asctime)s [%(levelname)s] - (Line: %(lineno)d [%(filename)s]) - %(message)s',
                                   datefmt='%Y-%m-%d %I:%M:%S %p')
 
-    file_handler = RotatingFileHandler('app.log', maxBytes=10 * 1024 * 1024, backupCount=5)
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
-
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
     console_handler.setFormatter(formatter)
 
-    logger.addHandler(file_handler)
     logger.addHandler(console_handler)
